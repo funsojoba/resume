2ac537686c1daaef9e58d2502dd01db30f9f5682
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-08-18 21:03:10 +0100

    last update before deployment

--- resume_app/views.py
+++ resume_app/views.py
@@ -1,5 +1,56 @@
+from django.core import mail
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from validate_email import validate_email
+
 
 
 def index(request):
+
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        context = {
+            "values":request.POST
+        }
+
+        if not name:
+            messages.error(request, 'Name is required please')
+            return render(request, 'index.html', context)
+        
+        if not email:
+            messages.error(request, 'email is required please')
+            return render(request, 'index.html', context)
+        
+        if not subject:
+            messages.error(request, 'subject is required please')
+            return render(request, 'index.html', context)
+        
+        if not message:
+            messages.error(request, 'message is required please')
+            return render(request, 'index.html', context)
+
+        if not validate_email(email):
+            messages.error(request, 'please enter a valid email')
+            return render(request, 'index.html', context)
+
+        senders_message = f'You have a message from {email}\n {message}'
+        
+        try:
+            send_mail(
+                subject,
+                senders_message,
+                email,
+                ['hrfunsojoba@gmail.com'],
+                fail_silently=False,
+            )
+            messages.success(request, 'Mail sent, thank you')
+            return render(request, 'index.html')
+        except Exception:
+            messages.error(request, 'Something went wrong')
+            return render(request, 'index.html')
     return render(request, 'index.html')
--- resume_project/settings.py
+++ resume_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,9 @@ STATIC_ROOT = '/static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
