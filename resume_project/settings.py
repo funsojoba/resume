0203beb84c9ed30f3c1635cc16c263a640018acc
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-08-18 21:18:48 +0100

    deployment settings

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -138,3 +139,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+
+
+django_heroku.settings(locals())
